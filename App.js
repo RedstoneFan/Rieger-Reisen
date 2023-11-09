cf6afe3ac9bc1a52e3f32a65cd94844b09c1cb58
RedstoneFan <150341295+RedstoneFan@users.noreply.github.com>
2023-11-09 17:31:26 +0100

    v01.5

--- App.js
+++ App.js
@@ -1,8 +1,22 @@
 function zufallZiel(){
     var zufall = Math.round(Math.random() * (7 - 1)) + 1;
+    var ziel ;
         console.log(zufall);
 
     switch(zufall) {
+        case 1: ziel = "Paris"; break;
+        case 2: ziel = "Rom"; break;
+        case 3: ziel = "London"; break;
+        case 4: ziel = "Strassburg"; break;
+        case 5: ziel = "Athen"; break;
+        case 6: ziel = "Pisa"; break;
+        case 7: ziel = "Barcelona"; break;
+        default: zufallZiel(); break;
+    }
+
+    if(confirm("Für Sie wurde " + ziel + " ausgelost. Wist du dort hin weiter geleitet werden?"))
+    {
+        switch(zufall) {
         case 1: window.location.href = "Paris.html"; break;
         case 2: window.location.href = "Rom.html"; break;
         case 3: window.location.href = "London.html"; break;
@@ -11,7 +25,13 @@ function zufallZiel(){
         case 6: window.location.href = "Pisa.html"; break;
         case 7: window.location.href = "Barcelona.html"; break;
         default: zufallZiel(); break;
+        }
     }
+    
 
     return;
+}
+
+function wichtig() {
+    alert("Diese webseite ist nur ein Schulprojekt und besteht, abgesehen von den zwei animierten Bildern, <nobr> aus eigenen  Aufnahmen. Die Empfehlungen sind nur meine Meinung und sind aus eigenen Erfahrungen. Die Informationen auf dieser Webseite können von der Realität abweichen und/oder nicht mehr Aktuell sein.");
 }
